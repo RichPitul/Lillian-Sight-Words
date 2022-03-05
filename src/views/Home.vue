b67499f38250f7a0e35c1a8304cfdb3f5936fdc3
Richard Pitul <richard@channel1media.com>
2022-03-05 13:04:31 -0500

    first push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div @click="changeCurrent">{{shuffledSightWords[current]}}</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue'
+const sightWords  = [
+    'like',
+    'a',
+    'i',
+    'he',
+    'and',
+    'in',
+    'that',
+    'of',
+    'is',
+    'it',
+    'to',
+    'can',
+    'the',
+    'was',
+    'big',
+    'no',
+    'he',
+    'said',
+    'go',
+    'my',
+    'and',
+    'yes',
+    'he',
+    'seen',
+    'went'
+];
+let shuffledSightWords = shuffle(sightWords);
+let current = ref(0);
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
   }
+
+  return array;
+}
+function changeCurrent(){
+    current.value++;
+    if(current.value == shuffledSightWords.length) {
+        current.value = 0;
+        shuffledSightWords = shuffle(sightWords);
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+div {
+    position: absolute;
+    top:0;
+    left:0;
+    display:flex;
+    width:100%;
+    height:100%;
+    align-items: center;
+    justify-content: center;
+    font-size: 20em;
+    user-select: none;
+}
+</style>
